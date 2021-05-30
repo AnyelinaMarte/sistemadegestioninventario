b0081d94d738d3c4d3ba1396e2330dd2e5146329
AnyelinaMarte <84147190+AnyelinaMarte@users.noreply.github.com>
2021-05-30 12:24:47 -0400

    Update Login.js

--- pages/Login.js
+++ pages/Login.js
@@ -1,5 +1,6 @@
 import DraftsIcon from '@material-ui/icons/Drafts';
 import LockIcon from '@material-ui/icons/Lock';
+import RoomIcon from '@material-ui/icons/Room';
 import style from '../styles/Login.module.css';
 import AccountBalanceIcon from '@material-ui/icons/AccountBalance';
 import {useState} from 'react';
@@ -10,12 +11,13 @@ export default function Login(){
         const nombreEmpresa = document.getElementById('nombre_Empresa').value;
         const correoAdmin = document.getElementById('correo_Admin').value;
         const claveAdmin = document.getElementById('clave_Admin').value;
+        const direccionEmpresa = document.getElementById('direccion_Empresa').value;
         auth.createUserWithEmailAndPassword(correoAdmin,claveAdmin)
         .then(async Usuario=>{
             
             await auth.onAuthStateChanged(async User=>{
                 if (User != null){
-            await db.collection('Usuario').doc(User.uid).collection('BD_Usuario').doc('datos_Usuario').set({nombreEmpresa})}
+            await db.collection('Usuario').doc(User.uid).collection('BD_Usuario').doc('datos_Usuario').set({nombreEmpresa, direccionEmpresa})}
             })
         })
 
@@ -45,6 +47,12 @@ export default function Login(){
                     <label>
                         <DraftsIcon style={{color:'#3C01F1', fontSize:'30px'}} />
                         <input type="email" id="correo_Admin" placeholder="Ingresa tu correo aqui"/>
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        <RoomIcon style={{color:'#3C01F1', fontSize:'30px'}} />
+                        <input type="text" id="direccion_Empresa" placeholder="Ingresa la direccion de la empresa"/>
                     </label>
                 </div>
                 <div>
