4c337a77131181b4ac9aafa00edd3cfc8bc49441
OrlinSamuelPerez <orlinsampa28@gmail.com>
2021-07-14 10:45:20 -0400

    Diseño admin

--- pages/Admin.js
+++ pages/Admin.js
@@ -6,13 +6,21 @@ import {useState} from 'react';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';  
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
 function TabPanel(props) {
     const { children, value, index, ...other } = props;
-  
+    const [state, setState] = useState({
+      checkedA: true,
+      checkedB: true,
+      checkedC: true,
+      checkedD: true,
+    });
     return (
       <div
         role="tabpanel"
-        hidden={value !== index}
+        hidden={value !== index} 
         id={`scrollable-force-tabpanel-${index}`}
         aria-labelledby={`scrollable-force-tab-${index}`}
         {...other}
@@ -61,27 +69,80 @@ export default function Admin(){
                         textColor="primary"
                         aria-label="scrollable force tabs example"
                     >
-                        <Tab label="General" {...a11yProps(0)} />
-                        <Tab label="Notificaciones"  {...a11yProps(1)} />
-                        <Tab label="Empleados"  {...a11yProps(2)} />
+                        <Tab label="Control Actividades"  {...a11yProps(0)} />
+                        <Tab label="Control empleados" {...a11yProps(1)} />
+                        <Tab label="Agregar Empleados"  {...a11yProps(2)} />
                     </Tabs>
                 </AppBar>
                     <TabPanel value={value} index={0}>
+                      <div>
+                          <h1 className="admin-title">Actividades </h1>
+                          <div>
+                            <form className="form-empleado actividades" >
+                                  <div>
+                                    <TextField style={{width:'100%'}} id="outlined-basic" label="Titulo Actividad" variant="outlined" />
+                                  </div>
+                                  <div>
+                                    <TextField style={{width:'100%'}} id="outlined-basic" label="Fecha de Actividad" variant="outlined" />
+                                  </div>
+                                  <div>
+                                    <textarea className="textT-Empleado" placeholder="Agrega una descripcion">
+
+                                    </textarea>
+                                  </div>
+                                 <div className="center-button"><button>Agregar Actividad</button> </div>
+                                  
+                            </form>
+                          </div>
+                        </div>
                     </TabPanel>
                     <TabPanel value={value} index={1}>
-                        Item Two
+                    
                     </TabPanel>
                     <TabPanel value={value} index={2}>
                         <div>
-                          <h1>Control de empleados</h1>
-                          <Button variant="contained" style={{background:'#5664D2', color:'white',fontWeight:'bold', borderRadius:'70px'}}>
-                            Agregar nuevo empleado
-                          </Button>
+                          <h1 className="admin-title">Agregar Nuevo Empleado</h1>
                           <div>
-                            <h2>Empleados acutalmente activos</h2>
-                            <div>
-                              
-                            </div>
+                            <form className="form-empleado" >
+                              <div className="grid-admin-empleado">
+                                  <div>
+                                    <TextField style={{width:'100%'}} id="outlined-basic" label="Nombres *" variant="outlined" />
+                                  </div>
+                                  <div>
+                                    <TextField style={{width:'100%'}} id="outlined-basic" label="Apellidos *" variant="outlined" />
+                                  </div>
+                                  <div>
+                                    <TextField style={{width:'100%'}} id="outlined-basic" label="Correo *" variant="outlined" />
+                                  </div>
+                                  <div>
+                                    <TextField style={{width:'100%'}} id="outlined-basic" label="Contraseña *" variant="outlined" />
+                                  </div>
+                                  <div>
+                                    <TextField style={{width:'100%'}} id="outlined-basic" label="Cargo *" variant="outlined" />
+                                  </div>
+                                  <div>
+                                    <TextField style={{width:'100%'}} id="outlined-basic" label="Fecha de entrada *" variant="outlined" />
+                                  </div>
+                              </div>
+                                  <span>Roles del empleado</span>
+                                  <div className="grid-check">
+                                    <div>
+                                        <FormControlLabel control={<Checkbox name="checkedA" />} label="Mantenimiento" />
+                                    </div>  
+                                    <div>
+                                        <FormControlLabel control={<Checkbox name="checkedB" />} label="Pedidos" />
+                                    </div>
+                                    <div>
+                                        <FormControlLabel control={<Checkbox name="checkedC" />} label="Estadistica" />
+                                    </div>
+                                    <div>
+                                        <FormControlLabel control={<Checkbox name="checkedD" />} label="Stock" />
+                                    </div>
+                                </div>
+
+                                 <div className="center-button"><button>Agregar Empleado</button> </div>
+                                  
+                            </form>
                           </div>
                         </div>
                     </TabPanel>
